397b980da26b31d029e968aeccaef199371622e0
rahaduzzaman <rahad1@ymail.com>
2019-11-26 23:29:35 -0500

    Progress make upto working with async data

--- functions.js
+++ functions.js
@@ -1,12 +1,20 @@
+const axios = require("axios");
 const functions = {
-    add: (num1, num2) => num1 + num2,
-    isNull: () => null,
-    chechValue: (x) => x,
-    createUser: () => {
-        const user = {firstName : 'MD'};
-        user['lastName'] = 'Rahaduzzaman';
-        return user;
-    }
-    }
+  add: (num1, num2) => num1 + num2,
+  isNull: () => null,
+  chechValue: x => x,
+  createUser: () => {
+    const user = {
+      firstName: "MD"
+    };
+    user["lastName"] = "Rahaduzzaman";
+    return user;
+  },
+  fetchUser: () =>
+    axios
+      .get("https://jsonplaceholder.typicode.com/users/1")
+      .then(res => res.data)
+      .catch(err => "error getting Data")
+};
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -1,4 +1,4 @@
-const functions = require('./functions');
+const functions = require("./functions");
 
 // CHECK FOR TRUTHY & FALSY VALUES
 // toBeNull matches only null
@@ -7,25 +7,56 @@ const functions = require('./functions');
 // toBeTruthy matches anything that an if statement treats as true
 // toBeFalsy matches anything that an if statement treats as false
 
-test('Add 2 + 2 to equal 4', () => {
-    expect(functions.add(2,2)).toBe(4);
+test("Add 2 + 2 to equal 4", () => {
+  expect(functions.add(2, 2)).toBe(4);
+});
+//toBe
+test("Add 2 + 2 to NOE equal 5", () => {
+  expect(functions.add(2, 2)).not.toBe(5);
+});
+//isNull
+test("Should Be Null", () => {
+  expect(functions.isNull()).toBeNull();
+});
+//CheckValue
+test("Should Be falsy", () => {
+  expect(functions.chechValue(null)).toBeFalsy();
+});
+// ToEqual
+test("Should Be Md Rahaduzzaman", () => {
+  expect(functions.createUser()).toEqual({
+    firstName: "MD",
+    lastName: "Rahaduzzaman"
+  });
+});
+// Less Than and Greater Than
+test("Should Be under or Equal 1600", () => {
+  const load1 = 800;
+  const load2 = 800;
+  expect(load1 + load2).toBeLessThanOrEqual(1600);
+});
+//Regular Expression
+test("There is no I in Team", () => {
+  expect("Team").not.toMatch(/I/i);
+});
+// Arrays
+test("Admin should be in usernames", () => {
+  usernames = ["john", " mark", "admin"];
+  expect(usernames).toContain("admin");
 });
 
-test('Add 2 + 2 to NOE equal 5', () => {
-    expect(functions.add(2,2)).not.toBe(5);
-});
+// Working with async data
 
-test('Should Be Null', () => {
-    expect(functions.isNull()).toBeNull();
-});
-
-test('Should Be falsy', () => {
-    expect(functions.chechValue(null)).toBeFalsy();
-});
-
-test('Should Be Md Rahaduzzaman', () => {
-    expect(functions.createUser()).toEqual({
-        firstName: 'MD',
-        lastName: 'Rahaduzzaman'
-    });
+//Promise
+// test("user fetch name should be Leanne Graham", () => {
+//   expect.assertions(1);
+//   return functions.fetchUser().then(data => {
+//     expect(data.name).toEqual("Leanne Graham");
+//   });
+// });
+//async
+test("user fetch name should be Leanne Graham", async () => {
+  expect.assertions(1);
+  const data = await functions.fetchUser();
+  expect(data.name).toEqual("Leanne Graham");
 });
